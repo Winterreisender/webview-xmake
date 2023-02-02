45639ff23308e82e9078ebf8dead09c326b70bf6
Winterreisender <winterreisender@foxmail.com>
2023-02-02 15:57:43 +0800

    try adding mingw support

--- source/xmake.lua
+++ source/xmake.lua
@@ -28,3 +28,28 @@ target("webview")
     add_files("src/webview.cpp")
     set_optimize("fast")
 target_end()
+
+target("webview_static")
+    set_kind("static")
+    set_languages("c++20")
+    add_packages("webview")
+    add_packages("MSWebview2 1.0.1518.46")
+    
+    if is_plat("linux") then
+        add_packages("pkgconfig::gtk+-3.0")
+        add_packages("pkgconfig::webkit2gtk-4.0")
+    end
+
+    if is_plat("windows")
+        add_cxxflags("/EHsc")
+        add_defines("WEBVIEW_API=__declspec(dllexport)")
+    end
+
+    if is_plat("macosx") then
+        set_languages("c++11")
+        add_frameworks("WebKit")
+    end
+
+    add_files("src/webview.cpp")
+    set_optimize("fast")
+target_end()
