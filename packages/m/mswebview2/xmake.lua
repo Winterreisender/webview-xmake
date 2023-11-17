9875ef3885701fd05344bfe0c57d93a8da1edf55
Winterreisender <winterreisender@foxmail.com>
2023-11-17 15:32:13 +0800

    update

--- packages/m/mswebview2/xmake.lua
+++ packages/m/mswebview2/xmake.lua
@@ -4,6 +4,7 @@ package("MSWebview2")
     set_license("https://www.nuget.org/packages/Microsoft.Web.WebView2/$(version)/License") -- BSD-Style
 
     add_urls("https://globalcdn.nuget.org/packages/microsoft.web.webview2.$(version).nupkg#dl.zip")
+    add_versions("1.0.2151.40", "64091274371a98a7b815dccd31363632f8ecbb08977b9f9119542932733e87e7")
     add_versions("1.0.1518.46", "63020b2d569d09a2098ae1ca20dd4cc281885f794aa00fc8812c6ab52dd49618")
 
     on_install(function (package)
--- packages/w/webview/xmake.lua
+++ packages/w/webview/xmake.lua
@@ -7,11 +7,12 @@ package("webview")
     set_urls("https://github.com/webview/webview.git")
 
     -- Add new versions ABOVE
+    add_versions("0.11.0.231117", "53ea174ce79ca2f52e28dd51d49052aebce3f4c5")
     add_versions("0.10.0.230210", "7b40e46d97e953a426ff553f92f2cc901cbf8bf9")
     add_versions("0.10.0.230202", "14f8e24a873c7e8deb2b1dd29c8e5841529fe467")
     
     if is_plat("windows") or is_plat("mingw") then
-        add_deps("MSWebview2 1.0.1518.46")
+        add_deps("MSWebview2 1.0.2151.40")
     end
     
     on_install(function (package)
